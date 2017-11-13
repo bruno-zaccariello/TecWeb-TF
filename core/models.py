cd0679b9b5e70d614e7d8b4e7a92bd073145d6b7
Bruno Zaccariello <brukzz90@gmail.com>
2017-11-13 21:26:59 -0200

    v.LoginAuth

--- core/models.py
+++ core/models.py
@@ -1,6 +1,72 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+ALUNO = "A"
+PROFESSOR = "P"
+COORDENADOR = "C"
+PERFIS = (
+    (ALUNO, 'Aluno'),
+    (PROFESSOR, 'Professor'),
+    (COORDENADOR, 'Coordenador')
+)
+
+class UsuarioManager(BaseUserManager):
+    use_in_migrations = True
+    
+    def _create_user(self, ra, password, **extra_fields):
+        if not ra:
+            raise ValueError("RA precisa ser preenchido")
+        user = self.model(ra=ra, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_user(self, ra, password=None, **extra_fields):
+        return self._create_user(ra, password, **extra_fields)
+    
+    def create_superuser(self, ra, password, **extra_fields):
+        return self._create_user(ra, password, **extra_fields)
+
+class Usuario(AbstractBaseUser):
+    ra = models.IntegerField("RA", unique=True)
+    nome = models.CharField("Nome", max_length=100, blank=True)
+    email = models.EmailField("E-mail", unique="True")
+    ativo = models.BooleanField("Ativo", default=True)
+    perfil = models.CharField("Perfil", max_length=1, choices=PERFIS)
+    
+    USERNAME_FIELD = "ra"
+    
+    objects = UsuarioManager()
+    
+    REQUIRED_FIELDS = ["nome", "email"]
+    
+    def get_full_name(self):
+        return self.nome
+    
+    def get_shot_name(self):
+        return self.nome
+    
+    def __str__(self):
+        return self.nome
+    
+    def has_perm(self, perm, obj=None):
+        return True
+    
+    def has_module_perms(self, app_label):
+        return True
+    
+    @property
+    def is_staff(self):
+        return self.perfil == 'C'
 
 # Create your models here.
+
+class Aluno(Usuario):
+    parent_link = models.OneToOneField(Usuario,primary_key=True,db_column="usuario_id",parent_link=True)
+    curso = models.ForeignKey('core.Curso',blank=True,null=True,)
+    celular = models.CharField("Celular", max_length=11)
+
+
 		
 class Curso(models.Model):
     
--- impacta/settings.py
+++ impacta/settings.py
@@ -84,6 +84,8 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = "core.Usuario"
+
 LOGIN_URL = 'login'
 
 LOGIN_REDIRECT_URL = 'home'
@@ -124,3 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
